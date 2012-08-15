fc59249d15e988221dd332f40886466dfdde0e36
Ondřej Koch <okoch@dns.cz>
2012-08-15 18:57:12 +0200

    Implemented domain creation
    Signed-off-by: Ondřej Koch <okoch@dns.cz>

--- ponyvirt/hypervisor.py
+++ ponyvirt/hypervisor.py
@@ -1,8 +1,14 @@
 import libvirt
+import os
 from ponyvirt.domain import Domain
+from xml.etree.ElementTree import ElementTree, Element, XML, tostring
+
+
+def disk_id_generator():
+    for i in xrange(26):
+        yield 'vd' + chr(ord('a') + i)
 
 class Hypervisor(object):
-
     def __init__(self, connection_string="qemu:///system"):
         """
         Connection string example qemu:///system
@@ -44,13 +50,55 @@ class Hypervisor(object):
     def close(self):
         self.conn.close()
 
-    def create(self):
-        pass
+    def create(self, name, uuid, memory=256, vcpu=1, disks=[], vifs=[]):
+        """
+        Creates new domain.
 
-    def __del__(self, item):
-        self[item].delete()
+        Disks are something like:
+            [
+                ('network', 'raw', 'sheepdog', 'vm:alice:root'),
+                ('network', 'raw', 'sheepdog', 'vm:alice:eph'),
+            ]
+
+        Virtual interfaces:
+            [
+                ('br100', 'fa:16:3e:73:3b:f8'),
+            ]
+        """
+        tmp_vm = ElementTree(
+            file=os.path.dirname(__file__) + "/templates/vm.xml")
+
+        tmp_vm.find("name").text = name
+        tmp_vm.find("uuid").text = uuid
+        tmp_vm.find("memory").text = str(memory)
+        tmp_vm.find("vcpu").text = str(vcpu)
+
+        diskseq = disk_id_generator()
+        for disk in disks:
+            tmp_disk = ElementTree(
+                file = os.path.dirname(__file__) + "/templates/disk.xml")
+            tmp_disk.getroot().attrib['type'] = disk[0]
+            tmp_disk.find("driver").attrib['type'] = disk[1]
+            tmp_disk.find("source").attrib['protocol'] = disk[2]
+            tmp_disk.find("source").attrib['name'] = disk[3]
+            tmp_disk.find("target").attrib['dev'] = next(diskseq)
+            tmp_vm.find("devices").append(tmp_disk.getroot())
+        for network in vifs:
+            tmp_net = ElementTree(
+                file=os.path.dirname(__file__) + "/templates/network.xml")
+            tmp_net.find("source").attrib["bridge"] = network[0]
+            tmp_net.find("mac").attrib["address"] = network[1]
+            tmp_vm.find("devices").append(tmp_net.getroot())
+        self.conn.createXML(tostring(tmp_vm.getroot()), 0)
+
+
+def __del__(self, item):
+    self[item].delete()
 
 
 if __name__ == "__main__":
     h = Hypervisor()
-    print h
+    h.create("Testicek", "d56f7b2c-e531-405f-8fb1-e37764970231",
+        disks=[('network', 'raw', 'sheepdog', 'vm:alice:root'),
+            ('network', 'raw', 'sheepdog', 'vm:alice:eph')],
+        vifs=[('virbr0', 'fa:16:3e:73:3b:f8')])
