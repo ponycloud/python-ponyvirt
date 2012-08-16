b778bf5d50512a80dee268afd1ff9bf4c2c19ee0
Ondřej Koch <okoch@dns.cz>
2012-08-16 15:59:22 +0200

    Added templates and XML builder
    Signed-off-by: Ondřej Koch <okoch@dns.cz>

---
+++ ponyvirt/virtxmlbuilder.py
@@ -0,0 +1,64 @@
+from xml.etree.ElementTree import TreeBuilder
+
+
+def sans(d, *items):
+    d = d.copy()
+    for i in items:
+        if i in d:
+            del d[i]
+    return d
+
+def generate_disk(disk, devicename):
+    """
+    Creates XML representation of disk for libvirt based on disk_definition
+
+    disk_definition is something like
+            {
+                'type': 'network',
+                'device': 'disk',
+                'format': 'raw',
+                'source': {
+                    'protocol': 'sheepdog',
+                    'name': 'Alice',
+                    'hosts': [('127.0.0.1', '7000'),],
+                },
+            }
+     or
+             {
+                'type': 'file',
+                'device': 'disk',
+                'format': 'qcow2',
+                'source': {
+                    'file': '/var/lib/libvirt/images/Alice.img',
+                },
+            },
+
+    devicename is string representing devicename (eg. vda, vdb, ...)
+    """
+
+    builder = TreeBuilder()
+    builder.start('disk',
+                {'type': disk['type'], 'device': disk['device']})
+    builder.start('source', sans(disk['source'], 'hosts'))
+    for host in disk['source'].get('hosts', []):
+        builder.start('host', {'name': host[0], 'port': host[1]})
+        builder.end('host')
+    builder.end('source')
+    builder.start('target', {'dev': devicename, 'bus': 'virtio'})
+    builder.end('target')
+    builder.start('driver', {'name': 'qemu', 'cache': 'none', 'type': disk['format']})
+    builder.end('driver')
+    builder.end('disk')
+    return builder.close()
+
+
+def generate_nic(network):
+    builder = TreeBuilder()
+    builder.start('interface', {'type': network['type']})
+    builder.start('mac', {'address': network['mac']})
+    builder.end('mac')
+    builder.start('source', network['source'])
+    builder.end('source')
+    builder.end('interface')
+    return builder.close()
+
